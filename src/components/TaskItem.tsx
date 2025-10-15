c27309c7ad42238864bb93b163bf3f239997ae55
clendson.goncalves@gmail.com <clendson.goncalves@gmail.com>
2025-10-14 23:32:35 -0300

    fix: task completed icon fixed

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Trash2, NotebookPen } from "lucide-react";
+import { Trash2, NotebookPen, CircleCheck, Circle } from "lucide-react";
 import React, { useState, useRef, useEffect } from "react";
 import { Task } from "../utils/utils";
 
@@ -45,44 +45,27 @@ export const TaskItem: React.FC<TaskItemProps> = ({
   }, []);
 
   return (
-    <li
-      ref={itemRef}
-      className={`flex items-center gap-3 py-2 px-2 group rounded-lg transition-colors ${
-        isNotesOpen ? "" : "hover:bg-gray-700/70"
-      } relative`}
-    >
-      <div className="w-full">
-        <div className="w-full flex gap-3 items-start">
+    <li ref={itemRef} className="py-2">
+      <div className="flex items-start justify-between">
+        <div className="flex items-start">
           <button
             onClick={(e) => {
               e.stopPropagation();
               onToggle(task.id);
             }}
-            aria-label="toggle task"
-            className={`w-4 h-4 rounded-full border transition-all ${
-              task.completed
-                ? "border-green-500 bg-green-500"
-                : `border-gray-600 ${isNotesOpen ? "" : "hover:border-sky-400"}`
-            }`}
+            aria-label={
+              task.completed ? "mark as incomplete" : "mark as complete"
+            }
+            className={`mr-2 p-1 rounded-full transition-colors focus:outline-none ${task.completed}`}
           >
-            {task.completed && (
-              <svg
-                className="w-2 h-2 mx-auto text-white"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={3}
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
+            {task.completed ? (
+              <CircleCheck size={18} className="text-teal-600" />
+            ) : (
+              <Circle size={18} className="text-gray-500" />
             )}
           </button>
 
-          <div className="flex-grow" onClick={() => onToggle(task.id)}>
+          <div className="min-w-0" onClick={() => onToggle(task.id)}>
             <div
               className={`cursor-pointer transition-all ${
                 task.completed ? "text-gray-500 line-through" : "text-gray-200"
@@ -97,7 +80,9 @@ export const TaskItem: React.FC<TaskItemProps> = ({
               </p>
             )}
           </div>
+        </div>
 
+        <div className="flex items-center">
           <button
             onClick={(e) => {
               e.stopPropagation();
@@ -138,42 +123,42 @@ export const TaskItem: React.FC<TaskItemProps> = ({
             )}
           </button>
         </div>
-
-        {isNotesOpen && (
-          <div className="w-full mt-4">
-            <textarea
-              value={editNotes}
-              onChange={(e) => setEditNotes(e.target.value)}
-              rows={4}
-              className="w-full bg-transparent text-white placeholder-gray-500 border border-gray-700 text-sm rounded-md p-2 focus:outline-none"
-              placeholder="Add notes for this task..."
-            />
-
-            <div className="mt-2 flex justify-end gap-2">
-              <button
-                onClick={() => {
-                  setIsNotesOpen(false);
-                  setEditNotes(task.notes ?? "");
-                }}
-                className="bg-gray-700 text-white p-1 rounded text-sm w-24 hover:opacity-50"
-              >
-                Cancel
-              </button>
-
-              <button
-                onClick={() => {
-                  if (typeof onSaveNotes === "function")
-                    onSaveNotes(task.id, editNotes);
-                  setIsNotesOpen(false);
-                }}
-                className="bg-teal-500 text-white p-1 rounded text-sm w-24 hover:opacity-50"
-              >
-                Save
-              </button>
-            </div>
-          </div>
-        )}
       </div>
+
+      {isNotesOpen && (
+        <div className="w-full mt-4">
+          <textarea
+            value={editNotes}
+            onChange={(e) => setEditNotes(e.target.value)}
+            rows={4}
+            className="w-full bg-transparent text-white placeholder-gray-500 border border-gray-700 text-sm rounded-md p-2 focus:outline-none"
+            placeholder="Add notes for this task..."
+          />
+
+          <div className="mt-2 flex justify-end gap-2">
+            <button
+              onClick={() => {
+                setIsNotesOpen(false);
+                setEditNotes(task.notes ?? "");
+              }}
+              className="bg-gray-700 text-white p-1 rounded text-sm w-24 hover:opacity-50"
+            >
+              Cancel
+            </button>
+
+            <button
+              onClick={() => {
+                if (typeof onSaveNotes === "function")
+                  onSaveNotes(task.id, editNotes);
+                setIsNotesOpen(false);
+              }}
+              className="bg-teal-500 text-white p-1 rounded text-sm w-24 hover:opacity-50"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      )}
     </li>
   );
 };
